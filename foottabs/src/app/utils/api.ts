24d771d9bec2a946ffd0a1d3b1f223d010456540
Johanne enama <48734085+jenama@users.noreply.github.com>
2024-07-29 22:48:49 -0400

    Update api.ts

--- foottabs/src/app/utils/api.ts
+++ foottabs/src/app/utils/api.ts
@@ -11,35 +11,7 @@ const apiClient = axios.create({
   })
 
 
-// var config = {
-//     method: 'get',
-//     url: 'https://v3.football.api-sports.io/players',
-//     headers: {
-//       'x-rapidapi-key': '855e56858f67858b3c7ea71204411c06',
-//       'x-rapidapi-host': 'v3.football.api-sports.io'
-//     }
-//   };
-  
-//   axios(config)
-//   .then(function (response) {
-//     console.log(JSON.stringify(response.data));
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
 
-//   export default config;
-
-// require('dotenv').config({ path: '.env.local' });
-
-// const apiKey = process.env.API_KEY;
-
-// const apiClient = axios.create({
-//   baseURL: 'https://v3.football.api-sports.io/players',
-//   headers: {
-//     'Authorization': `Bearer ${apiKey}`,
-//   },
-// });
 
 export default apiClient;
 
